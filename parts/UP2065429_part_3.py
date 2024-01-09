bc759d4976a442c278634b18abd4f5f4bf1666a8
Jude <111361323+JudeWhiting@users.noreply.github.com>
2024-01-09 14:50:56 +0000

    Update UP2065429_part_3.py

--- parts/UP2065429_part_3.py
+++ parts/UP2065429_part_3.py
@@ -21,9 +21,12 @@ def train_neuron(inputs, target_outputs, W, B, epochs=100, learning_rate = 0.1):
 
 
 
-weights = weights[0]
-biases = biases[0]
+training_inputs = np.array([[np.random.rand()/2 for _ in range(2)] for _ in range(1000)])  #this creates a training set of inputs
+targets = np.array([[i[0] * i[1]] for i in training_inputs])     #this creates a training set of outputs
+
 data = np.array([[0,0],[0,1],[1,0],[1,1]]) #training data
 OR_truth_table = np.array([0,1,1,1]) #truth table for OR
-weights, biases = train_neuron(data, OR_truth_table, weights, biases)
-print(neuron(data, weights, biases))
+for i in range(HL[0]):   
+    weights[i], biases[i] = train_neuron(data, OR_truth_table, weights[i], biases[i])
+
+
